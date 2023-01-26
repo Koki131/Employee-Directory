e4ca5a29390e9221627d88e40fee9ded1bf93eef
koki131 <zoran.stevanovich94@gmail.com>
2023-01-26 15:39:50 +0100

    First commit

---
+++ src/main/java/com/employeedir/demo/EmployeeDirApplication.java
@@ -0,0 +1,13 @@
+package com.employeedir.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeDirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeDirApplication.class, args);
+	}
+
+}
