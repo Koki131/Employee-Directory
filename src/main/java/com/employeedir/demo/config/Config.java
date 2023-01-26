9871525cabc2de8d34b07d6b5bc98a18a123ea6e
koki131 <zoran.stevanovich94@gmail.com>
2023-01-26 15:50:21 +0100

    Added resource handler for employee images

---
+++ src/main/java/com/employeedir/demo/config/Config.java
@@ -0,0 +1,25 @@
+package com.employeedir.demo.config;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class Config implements WebMvcConfigurer {
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+		Path imageUploadDir = Paths.get("./employee-images/");
+		
+		String imageUploadPath = imageUploadDir.toFile().getAbsolutePath();
+		
+		registry.addResourceHandler("/employee-images/**").addResourceLocations("file://" + imageUploadPath + "/");
+	}
+
+	
+	
+}
