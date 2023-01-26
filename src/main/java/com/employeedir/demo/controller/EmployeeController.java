43c266d66377116f14c11957b5396f1b37f5b586
koki131 <zoran.stevanovich94@gmail.com>
2023-01-26 16:20:20 +0100

    Implemented the create functionality

--- src/main/java/com/employeedir/demo/controller/EmployeeController.java
+++ src/main/java/com/employeedir/demo/controller/EmployeeController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.employeedir.demo.entity.Employee;
 import com.employeedir.demo.service.EmployeeService;
@@ -25,7 +27,23 @@ public class EmployeeController {
 		
 		model.addAttribute("employees", employees);
 		
-		return "/employees/employee-list";
+		return "/employees/employee-list";	
+	}
+	
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model model) {
+		
+		model.addAttribute("employee", new Employee());
+		
+		return "/employees/employee-form";
+	}
+	
+	@PostMapping("/addEmployee")
+	public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+		
+		employeeService.save(employee);
+		
+		return "redirect:/";
 		
 	}
 }
